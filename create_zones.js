e1948e94ac32348dcba0adcfb81a9f6ca189292d
Chris Young <chris@worldofchris.com>
2021-06-16 21:29:58 +0100

    Fixed up zone creation

--- create_zones.js
+++ create_zones.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const city_size = 1024 * 100;
+const city_size = 1024 * 17;
 const num_zones = 16;
 const zone_size = Math.floor(city_size / num_zones);
 
@@ -13,15 +13,34 @@ for (var i = 0; i < zones.length; i++) {
 
 let rawdata = fs.readFileSync('city.json');
 let unsorted = JSON.parse(rawdata);
-unsorted.forEach((element) => {
-  // Put in row based on start x
-  const zone_x = Math.floor(element.geometry.start.x / zone_size)
-  const zone_y = Math.floor(element.geometry.start.y / zone_size)
-  if (zones[zone_x][zone_y] === undefined) {
-    zones[zone_x][zone_y] = {'streets': [element]};
-  } else {
-    zones[zone_x][zone_y].streets.push(element);
-  }
+
+unsorted.forEach((row) => {
+  row.forEach((element) => {
+    // Put in row based on start x
+    const zone_x = Math.floor(element.geometry.start.x / zone_size) + 1
+    const zone_y = Math.floor(element.geometry.start.y / zone_size) + 1
+    console.log(zone_x, zone_y)
+    if (zones[zone_x][zone_y] === undefined) {
+      zones[zone_x][zone_y] = {'streets': [element]};
+    } else {
+      zones[zone_x][zone_y].streets.push(element);
+    }
+  })
+})
+
+zones.forEach((row) => {
+  let row_text = ":";
+  let marker;
+  for (var i=0; i < row.length; i++) {
+    if (row[i] === undefined) {
+      marker = '-';
+    } else {
+      marker = '*';
+    }
+    row_text = row_text.concat(marker);
+
+}
+  console.log(row.length, row_text);
 })
 
 const zone_data = JSON.stringify(zones);
